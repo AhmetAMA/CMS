42a86c41737b03ceff5cfcfffd7039ad46811b2c
AhmetAMA <91487419+AhmetAMA@users.noreply.github.com>
2023-04-18 13:15:00 +0200

    OOP gemaakt
    De php code voor logout veranderd naar OOP

--- logout.php
+++ logout.php
@@ -1,12 +1,33 @@
-
+<?php ob_start(); ?>
 <?php session_start(); ?>
+
+<?php 
+// $_SESSION['username'] = null;
+// $_SESSION['firstname'] = null;
+// $_SESSION['lastname'] = null;
+// $_SESSION['user_role'] = null;
+
+// header("Location: ../index.php");
+?>
 
 <?php
 
+class Logout {
+    public function __construct() {
+        $this->logout();
+    }
+    
+    public function logout() {
+        $_SESSION['username'] = null;
+        $_SESSION['firstname'] = null;
+        $_SESSION['lastname'] = null;
+        $_SESSION['user_role'] = null;
 
-$_SESSION['username'] = null;
-$_SESSION['firstname'] = null;
-$_SESSION['lastname'] = null;
-$_SESSION['user_role'] = null;
+        header("Location: ../index.php");
+        exit();
+    }
+}
 
-header("Location: ../index.php");
+$logout = new Logout();
+
+?>
